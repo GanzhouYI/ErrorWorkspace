1ce6332fb9aacdd69a5ee113065851469d520b63
yiroote <laiyibin@laiyibindemacbook-pro.local>
2016-03-20 20:47:39 +0800

    Initial Commit

---
+++ MYFrame/MYFrame.h
@@ -0,0 +1,19 @@
+//
+//  MYFrame.h
+//  MYFrame
+//
+//  Created by __________V|R__________ on 16/3/20.
+//  Copyright © 2016年 __________V|R__________. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MYFrame.
+FOUNDATION_EXPORT double MYFrameVersionNumber;
+
+//! Project version string for MYFrame.
+FOUNDATION_EXPORT const unsigned char MYFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MYFrame/PublicHeader.h>
+
+
